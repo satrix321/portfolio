a306f5d47aa01758002917f1a2c3e35016386541
Krzysztof Szot <krzysztof.szot@PL-MAC-C02YT1Z3LVDM.local>
2020-09-20 16:14:12 +0200

    Remove intersection observer from footer component

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer" :class="{ 'footer--appear': animationStarted }">
+  <footer class="footer">
     <p>&copy; 2020 Krzysztof Szot</p>
   </footer>
 </template>
@@ -7,28 +7,6 @@
 <script>
 export default {
   name: 'TheFooter',
-  data: function () {
-    return {
-      animationStarted: false,
-    };
-  },
-  mounted: function () {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5
-    }
-    const observer = new IntersectionObserver((entries, observer) => {
-      console.log(entries[0].isIntersecting);
-      console.log(entries[0]);
-      if (!this.animationStarted && entries[0].isIntersecting) {
-        this.animationStarted = true;
-      }
-    }, options);
-
-    const target = this.$el;
-    observer.observe(target);
-  }
 };
 </script>
 
@@ -39,11 +17,5 @@ export default {
   justify-content: center;
   background: $primary-dark-2-color;
   color: white;
-  opacity: 0;
-  transition: opacity 1s;
-
-  &.footer--appear {
-    opacity: 1;
-  }
 }
 </style>
